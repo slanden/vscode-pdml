9a2b295fa9bf00dd302833cee0176c6536d59ea7
slanden <slanden523@gmail.com>
2025-12-07 22:14:34 -0600

    upgrade plugin interface, minify code,  remove unused dependencies

--- pdml-extensions/rolldown.config.mjs
+++ pdml-extensions/rolldown.config.mjs
@@ -1,39 +1,34 @@
 import { defineConfig } from "rolldown";
 import fs from "node:fs/promises";
 
-const items = [
+const OUT_DIR = "dist";
+const PLUGIN_DIR = `${OUT_DIR}/plugins`;
+
+fs.mkdir(PLUGIN_DIR, { recursive: true });
+const items = defineConfig([
 	{
 		input: "completions.mjs",
 		output: {
-			dir: "dist",
+			dir: OUT_DIR,
 			entryFileNames: "completions.mjs",
+			minify: true,
 		},
 		platform: "node",
 	},
 	{
 		input: "main.cjs",
 		output: {
-			dir: "dist",
+			dir: OUT_DIR,
 			entryFileNames: "main.cjs",
 			format: "cjs",
+			minify: true,
 		},
 		define: {
 			"process.env.IS_PROD": "true",
 		},
+		external: "vscode",
 	},
-	// The JS file generated from JCO relies on this but the bundler
-	// doesn't include it, probably because of the syntax it used for
-	// a dynamic import:
-	// `fileURLToPath(new URL("./worker-thread.js", import.meta.url))`
-	{
-		input:
-			"node_modules/@bytecodealliance/preview2-shim/lib/io/worker-thread.js",
-		output: {
-			dir: "dist/plugins",
-			format: "cjs",
-		},
-	},
-];
+]);
 const entries = await fs.readdir("wasi-modules");
 for (const entry of entries) {
 	if (!entry.endsWith(".mjs")) continue;
@@ -41,10 +36,10 @@ for (const entry of entries) {
 	items.push({
 		input: `wasi-modules/${entry}`,
 		output: {
-			dir: "dist/plugins",
+			dir: PLUGIN_DIR,
 			entryFileNames: entry,
 		},
 		platform: "node",
 	});
 }
-export default defineConfig(items);
+export default items;
